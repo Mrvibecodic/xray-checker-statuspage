d5e6fed9a98d9e283c81bc5989bd1796b5dc3184
Mrvibecodic <junkmailforjunk@protonmail.com>
2026-08-12 20:17:49 +0300

    poller: tests open the DB via storetest

--- internal/poller/ping_test.go
+++ internal/poller/ping_test.go
@@ -1,16 +1,16 @@
 package poller
 
 import (
-	"path/filepath"
 	"testing"
 
 	"xray-status/internal/checker"
 	"xray-status/internal/config"
 	"xray-status/internal/store"
+	"xray-status/internal/storetest"
 )
 
 func TestDetectPingEvents(t *testing.T) {
-	st, err := store.Open("sqlite", filepath.Join(t.TempDir(), "t.db"))
+	st, err := store.Open(storetest.DSN(t))
 	if err != nil {
 		t.Fatal(err)
 	}
